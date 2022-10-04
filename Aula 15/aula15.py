7d3350db05b5bb1c25a014d486e5758e571bff3e
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-10-04 00:05:14 -0300

    Aula nova

---
+++ Aula 15/aula15.py
@@ -0,0 +1,29 @@
+''' Formatando valores com modificadores
+
+:s - Texto (str)
+:d - Inteiros (int)
+:f - Números de ponto flutuante (float)
+:.(Número)f - Quantidade de casas decimais (float)
+:(caractere)(> ou < ou ^)(Quantidade)(Tipo - s, d ou f)
+
+> - Esquerda
+< - Direita
+^ - Centro
+'''
+
+num_1 = 10
+# num_2 = 3
+# divisao = num_1 / num_2
+# print( '{:.2f}'. format(divisao))
+
+# print(f'{num_1:0>10}')
+#
+# num_2 = 1150
+# print(f'{num_2:0>10}')
+
+nome = 'Patricia Carvalho'
+print(nome.lower()) #tudo minusculo
+print(nome.upper()) #tudo maiusculo
+print(nome.title()) #primeiras letras maiusculas
+
+#próxima aula 33
