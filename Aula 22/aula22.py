2c2fdd8f45b39c7e9d89cd9360358f04d869ee65
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-10-07 16:27:14 -0300

    Nova Aula

---
+++ Aula 22/aula22.py
@@ -0,0 +1,23 @@
+'''
+For / Else  em Python
+'''
+
+variavel = ['Patrícia Carvalho', 'Roberto Camargo', 'Steve', 'Gamora']
+
+# for valor in variavel:
+#     if valor.startswith('G'):   #startswith é para ver se a str começa com a letra escolhida
+#         print('Começa com G', valor)
+#     else:
+#         print('Não começa com G', valor)
+
+comeca_com_j = False
+for valor in variavel:
+    if valor.lower().startswith('j'):#lower converte a levra parea minuscula, assim consegue achar Maiusucla e Minuscula.
+        comeca_com_j = True
+
+    if comeca_com_j:
+        print('Existe uma palavra que começa com J.')
+    else:
+        print('Não existe uma palavra com J.')
+
+   #proxima aula 41
